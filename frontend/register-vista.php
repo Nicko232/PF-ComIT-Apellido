0593bc59fd58ea4a14feaf1aa9ca6b2689ca4b2e
Nicolás Mansilla <38636454+Nicko232@users.noreply.github.com>
2018-07-19 23:24:10 -0300

    Update register-vista.php

--- frontend/register-vista.php
+++ frontend/register-vista.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="utf-8">
-    <title>Login / Register Magtimus</title> 
+    <title>Login / Register Mundial</title> 
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">  
     <link rel="stylesheet" href="icon/style.css">
     <link rel="stylesheet" href="css/style.css"> 
